805221b9ad9ce0af5bd5be63c29b42f741d42d40
shkim5164 <gk457@naver.com>
2021-03-21 21:59:10 +0900

    2021-03-21 리액트 200 커밋

---
+++ sunukim/react200/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Props from './R017_Props'
+
+function App(){
+  return(
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <Props props_val="THIS IS PROPS"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sunukim/react200/client/src/R009_Es6.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class R009_Es6 extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount(){
+        var jsString1 = '자바스크립티';
+        var jsString2 = '입니다 \n 다음 줄입니다.';
+        console.log(jsString1 + ' 문자열'+ jsString2+'~');
+
+        var Es6String1 = 'ES6';
+        var Es6String2 = '입니다';
+        console.log(`${Es6String1} 문자열${Es6String2}!!
+--------다음 줄입니다`);
+
+        var LongString = "ES6에 추가된 String 함수들입니다.";
+        console.log('startsWith : '+LongString.startsWith("ES6에 추가"));
+        console.log('endsWith : '+LongString.endsWith("함수들입니다."));
+        console.log('includes : '+LongString.includes("추가된 String"));
+    }
+
+    render(){
+        return(
+            <h2>[THIS IS ES6 STRING]</h2>
+        )
+    }
+}
+
+export default R009_Es6;
