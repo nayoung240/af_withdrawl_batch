266f6814fcc299b80ac9a14358282efb69732875
SJLee <zzanggu3026@naver.com>
2021-03-25 21:34:35 +0900

    2021-03-25 chapter1 의 10까지 App.js

--- seungjae/src/App.js
+++ seungjae/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Test from './lab_010325_component_use';
+import LifeCycle from './chapter01/lab_05_variable_type';
 
 function App(){
   return (
     <div>
-      <h1>승쟝 세상</h1>
-      <p>HTML 적용 시키기 </p>
-      <Test></Test>
+      <LifeCycle
+      prop_value = 'FromApp.js'></LifeCycle>
     </div>
   );
 }
