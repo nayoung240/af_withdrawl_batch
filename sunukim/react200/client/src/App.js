342ee757d8cf03e7e092f79db4bbe7a328f5560c
shkim5164 <gk457@naver.com>
2021-03-25 21:55:10 +0900

    2021-03-25 react 200 66 까지

--- sunukim/react200/client/src/App.js
+++ sunukim/react200/client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import FetchPost from './R060_FetchPost'
+import ReactonClick from './R066_onClick'
 
 function App(){
   return(
     <div>
       <h1>Start React 200!</h1>
-      <p>CSS 적용하기</p>
-      <FetchPost/>
+      <ReactonClick/>
     </div>
   );
 }
