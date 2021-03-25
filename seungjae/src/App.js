b06f17e0e848c3a57415c823f99bfce9ba16c0d1
SJLee <zzanggu3026@naver.com>
2021-03-25 20:14:26 +0900

    1. .jsx애 html 적용하기

--- seungjae/src/App.js
+++ seungjae/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>승쟝 세상</h1>
+      <p>HTML 적용 시키기 </p>
     </div>
   );
 }
