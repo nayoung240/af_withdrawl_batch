9d0647f3f6e032c4cc2100cfaffcf0b8aeff3265
SJLee <zzanggu3026@naver.com>
2021-03-25 21:33:53 +0900

    파일명 변경

---
+++ seungjae/src/chapter01/lab_01_component_use.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class lab_010325_component_use extends Component {
+    render() {
+        return(
+            <h2> This is Imported Component </h2>
+        )
+    }
+}
+
+export default lab_010325_component_use;
