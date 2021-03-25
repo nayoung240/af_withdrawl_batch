470fa0adeeb10f9d43899a6ca2abf2c6704121e2
SJLee <zzanggu3026@naver.com>
2021-03-25 20:27:12 +0900

    Class Conponent 적용

--- seungjae/src/App.js
+++ seungjae/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Test from './lab_010325_component_use';
+
 function App(){
   return (
     <div>
       <h1>승쟝 세상</h1>
       <p>HTML 적용 시키기 </p>
+      <Test></Test>
     </div>
   );
 }
