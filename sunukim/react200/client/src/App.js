488fde9328589ecd1f4b9afd6bf9a465006c3d5b
shkim5164 <gk457@naver.com>
2021-03-29 23:40:20 +0900

    2021-03-29 ~83까지

--- sunukim/react200/client/src/App.js
+++ sunukim/react200/client/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import StrAddButton from './StrAddButton'
 
 class App extends Component {
@@ -6,12 +7,23 @@ class App extends Component {
     return(
       <div>
         <h1>Start React 200!</h1>
-        <span>{this.props.store.getState().data.str}</span>
+        {/* <span>{this.props.store.getState().data.str}</span> */}
+        {/* <br/> */}
+        <span>{this.props.str}</span>
         <br/>
-        <StrAddButton store={this.props.store} />
+        {/* <StrAddButton store={this.props.store} /> */}
+        <StrAddButton AppProp="200" />
       </div>
     );
   }
 }
 
+let mapStateToProps = (state, props) => {
+  console.log('Props from index.js : ' + props.indexProp)
+  return {
+    str : state.data.str
+  }
+}
+
+App = connect(mapStateToProps, null)(App)
 export default App;
