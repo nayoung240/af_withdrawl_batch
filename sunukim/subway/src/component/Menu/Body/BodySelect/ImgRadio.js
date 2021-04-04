ac743d6cc1cd4d0a17bb9cc5b34d0e44c7956fe4
shkim5164 <gk457@naver.com>
2021-04-04 16:04:51 +0900

    2021-04-04 데이터 뿌려주기

--- sunukim/subway/src/component/Menu/Body/BodySelect/ImgRadio.js
+++ sunukim/subway/src/component/Menu/Body/BodySelect/ImgRadio.js
@@ -3,7 +3,26 @@ import React from 'react';
 const ImgRadio = (props) => {
 
     const cardClick = (e) => {
-        console.log(e)
+        e.stopPropagation();
+        console.log(e.target.parentNode);
+        let target = (e.target.classList.contains('card-text')) ? e.target.parentNode.parentNode : e.target.parentNode;
+        let bIsOn = target.classList.contains('on');
+        console.log(target.children);
+        let 
+        for()
+        // let radioInput = target.children.getElementsByTagName('input[type="radio"]')
+        if(bIsOn){
+            target.classList.remove('on');
+            // radioInput.setAttribute('checked', false);
+        }
+        else{
+            let cards = document.getElementsByClassName('card');
+            for(let i = 0 ; i < cards.length; i++){
+                cards[i].classList.remove('on')
+            }
+            target.classList.add('on');
+            // radioInput.setAttribute('checked', true);
+        }
         // if(this.hasClass("on")){
         //     $(this).removeClass("on");
         //     $(this).children(".radio_btn").prop('checked', false);
@@ -18,8 +37,8 @@ const ImgRadio = (props) => {
 
     const sandWitchArr = props.data.map((sandwitch, index) => {
         return(
-                <div key={sandwitch.idx} className="card_area col-lg-4 col-md-6 col-sm-12">
-                    <div onClick={cardClick} className="card mx-auto rounded menu" style={{width: '18rem'}}>
+                <div onClick={cardClick} key={sandwitch.idx} className="card_area col-lg-4 col-md-6 col-sm-12">
+                    <div  className="card mx-auto rounded menu" style={{width: '18rem'}}>
                         <img src={sandwitch.image} className="card-img-top" alt={sandwitch.name}/>
                         <div className="card-body">
                             <h4 className="card-text center_align">{sandwitch.name}</h4>
