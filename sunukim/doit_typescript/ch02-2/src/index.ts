c1716b862309aba43c93d11baf6a1b1976175e69
shkim5164 <gk457@naver.com>
2021-04-14 21:58:50 +0900

    2021-04-14

--- sunukim/doit_typescript/ch02-2/src/index.ts
+++ sunukim/doit_typescript/ch02-2/src/index.ts
@@ -1,9 +1,16 @@
-import {IPerson, makePerson} from './person/Person'
+import IPerson from './person/IPerson'
+import Person, {makePerson} from './person/Person'
+import Chance from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = (): void => {
-    let jane: IPerson = makePerson('Jane')
-    let jack: IPerson = makePerson("Jack")
-    console.log(jane, jack)
-}
+const chance = new Chance()
+let persons: IPerson[] = R.range(0,2).map((n : number) => new Person(chance.name(), chance.age()))
+console.log(persons)
 
-testMakePerson()
+// const testMakePerson = (): void => {
+//     let jane: IPerson = makePerson('Jane')
+//     let jack: IPerson = new Person("Jack")
+//     console.log(jane, jack)
+// }
+
+// testMakePerson()
