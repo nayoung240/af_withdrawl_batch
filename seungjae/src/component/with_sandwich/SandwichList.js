94c15ddf1e49671b2206e781d23b62f039d295a1
seungjae32 <dldortmvl898@gmail.com>
2021-04-17 17:52:22 +0900

    샌드위치 종류 추가

---
+++ seungjae/src/component/with_sandwich/SandwichList.js
@@ -0,0 +1,43 @@
+import React ,{useState} from 'react';
+import LeftSelectMenu from './LeftSelectMenu';
+import PageNation from '../action/pagenation';
+import main_menu from './main_menu';
+import SandwichLen from './SandwichLen';
+
+const menu_list = [
+    "bmt", "italy", "meet", "chicken", "beef"
+];
+
+const SandwichList = () => {
+    
+    const [sandwich, setSandwich] = useState([]);
+    const [checkList, setCheckList] = useState([false, false, false, false, false]);
+
+    const handleChangeCheck = (index) => {
+        let temp = checkList.map((value, idx) => (
+            index === idx ? !value : false
+        ));
+
+        sandwich.pop();
+        sandwich.push(menu_list[index]);
+
+        setCheckList(temp);
+        setSandwich(sandwich);
+    }
+
+    return(
+        <div className="menu">
+            <ul>
+                {menu_list.map((value, idx) => (
+                    <li key={idx}>
+                        <input type="checkbox" checked={checkList[idx]} onChange={ () => handleChangeCheck(idx)}/>{value}
+                    </li>
+                ))}
+            </ul>
+            <LeftSelectMenu menu_list={sandwich} />
+            <PageNation prev="/sandwich/main" next="/sandwich/len"/>
+        </div>
+    );
+}
+
+export default SandwichList;
