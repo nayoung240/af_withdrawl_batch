21818d0cb50ced45063a89d61f028e71b0106f5e
seungjae32 <dldortmvl898@gmail.com>
2021-04-21 21:04:58 +0900

    Route 파일네임 변경

--- seungjae/src/component/Routes.js
+++ seungjae/src/component/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import main_menu from './with_sandwich/main_menu'
+import MainMenu from './with_sandwich/MainMenu'
 import SandwichLen from './with_sandwich/SandwichLen'
 
 const Routes = () => {
     return (
     	<Router>
-          <Route path="/sandwich/main" component={main_menu} />
+          <Route path="/sandwich/main" component={MainMenu} />
           <Route path="/sandwich/len" component={SandwichLen} />
         </Router>
     );
